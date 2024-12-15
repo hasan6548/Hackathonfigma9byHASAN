dccd505a282a29183c19fa3d93d1c6613ee462ee
Hasan Faisal <147795675+hasan6548@users.noreply.github.com>
2024-12-15 13:11:08 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -74,10 +74,10 @@ export default function Navbar() {
             <Image src="/search.png" alt="search" width={24} height={24} />
           </Link>
           <Link href="/signup" className="text-white hover:text-[#FF9F0D] transition-colors">
-            <Image src="/user.png" alt="user" width={24} height={24} />
+            <Image src="/User.png" alt="user" width={24} height={24} />
           </Link>
           <Link href="#" className="text-white hover:text-[#FF9F0D] transition-colors">
-            <Image src="/tote.png" alt="cart" width={24} height={24} />
+            <Image src="/Tote.png" alt="cart" width={24} height={24} />
           </Link>
         </div>
       </nav>
